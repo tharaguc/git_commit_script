201d3f8059fd23682eb2f7d7d4144e77462a5d62
tsk.haraguchi@gmail.com <tsk.haraguchi@gmail.com>
2022-06-01 11:06:45 +0900

    [mod] style

--- commit.sh
+++ commit.sh
@@ -19,7 +19,7 @@ if [ "$YN" = "y" ]; then
 	while [ "$COMMITOK" != "y" ]
 	do
 		echo "========================="
-		read -p $'\e[33;36;1mCommit message :\e[m' MSG
+		read -p $'\e[33;36;1mCommit message : \e[m' MSG
 		echo -e ">> \033[0;36mgit commit -m \"$MSG\"\033[0;39m"
 		read -p "OK? (y/n) " COMMITOK
 	done
@@ -34,7 +34,7 @@ fi
 read -p $'\e[33;46;1mDo you want to push? (y/n) \e[m' YN2
 
 if [ "$YN2" = "y" ]; then
-	read -p $'\e[33;36;1mrepo & branch >>  \e[mgit push ' ARG
+	read -p $'\e[33;36;1mrepo & branch >> git push : \e[m' ARG
 	echo "========================="
 	echo -e "\033[0;36mgit push $ARG\033[0;39m"
 	git push $ARG
