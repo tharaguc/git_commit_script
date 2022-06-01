ecc2b226f27506bade58f0a95765d50cb48943e7
tsk.haraguchi@gmail.com <tsk.haraguchi@gmail.com>
2022-06-01 14:02:58 +0900

    [fix] install dir

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-DIR=`echo $PATH | tr ":" " " | awk '{print$1}'`
-
 read -p "Command name for this script : " NAME
 
 cp commit.sh ./$NAME
 
-mv $NAME $DIR/
+mv $NAME /usr/local/bin/
 
-echo "Move \"$NAME\" into $DIR/"
+echo "Move \"$NAME\" into /usr/local/bin/"
 echo "------------------------------------------------"
 echo "Usage: $> $NAME [files to commit (accept \".\")]"
 echo "------------------------------------------------"
 echo -e "\033[0;33mYou can always use this command !\033[0;39m"
+
+rm -rf ../git_commit_script
