573cd450ee90b8cd314acb4985621cc9e3c3a6d5
tsk.haraguchi@gmail.com <tsk.haraguchi@gmail.com>
2022-06-01 11:21:50 +0900

    [change] image

--- commit.sh
+++ commit.sh
@@ -3,14 +3,17 @@
 for x in $@
 do
 	echo "========================="
-	echo "git add $x"
+	echo -e "\033[0;36mgit add $x\033[0;39m"
 	echo "========================="
 	git add $x
 done
 
+echo "---"
 git status
+echo "---"
 
-read -p "Do you want to commit? (y/n) " YN
+read -p $'\e[33;46;1mDo you want to commit? (y/n) \e[m' YN
+
 
 COMMITOK="n"
 
@@ -18,23 +21,24 @@ if [ "$YN" = "y" ]; then
 	while [ "$COMMITOK" != "y" ]
 	do
 		echo "========================="
-		read -p "Commit message :" MSG
-		echo "git commit -m \"$MSG\""
+		read -p $'\e[33;36;1mCommit message : \e[m' MSG
+		echo -e ">> \033[0;36mgit commit -m \"$MSG\"\033[0;39m"
 		read -p "OK? (y/n) " COMMITOK
 	done
 	echo ">>>>>>>>>>>>>>>>>>>>>>"
 	git commit -m "$MSG"
 	echo ">>>>>>>>>>>>>>>>>>>>>>"
 else
+	git reset HEAD
 	exit
 fi
 
-read -p "Do you want to push? (y/n) " YN2
+read -p $'\e[33;46;1mDo you want to push? (y/n) \e[m' YN2
 
 if [ "$YN2" = "y" ]; then
-	read -p "git push " ARG
+	read -p $'\e[33;36;1mrepo & branch >> git push : \e[m' ARG
 	echo "========================="
-	echo "git push $ARG"
+	echo -e "\033[0;36mgit push $ARG\033[0;39m"
 	git push $ARG
 	echo "========================="
 else
